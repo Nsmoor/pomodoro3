f1e191420f3e8f97e60fab4da609fe5171135551
nsmoor <j.j.smoor@student.tudelft.nl>
2021-03-25 09:56:38 +0000

    added a start and a stop

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-# Import and setup logging
+# Import 
 import logging
+from pomodoro import Pomodoro
+import signal
+
+#setup logging
 log_format = "[%(levelname)s] %(asctime)s [%(module)s:%(lineno)d] %(message)s"
 logging.basicConfig(format=log_format, level=logging.DEBUG)
+
+def handle_control_c(signal, frame):
+  pomodoro.stop()
+  exit()
+
+
+#construct a pomodoro object
+pomodoro = Pomodoro()
+pomodoro.start()
+signal.signal(signal.SIGINT, handle_control_c)
--- pomodoro.py
+++ pomodoro.py
@@ -1,35 +1,67 @@
-# Import 
+# Import logging
 import logging
+from threading import Timer
 
-#setup pomodoro class
-class pomodoro:
-  def __init__():
-    logging.debug("Construct")
+#states
+IDLE = 0
+WORK = 1
+BREAK_ALARM = 2
+SHORT_BREAK = 3
+WORK_ALARM = 4
+LONG_BREAK = 5
 
-  def setup():
-    logging.debug("Setup")
-
-  def isPressed():
-    logging.debug("isPressed")
-
-  def strartWork():
-    logging.debug("strartWork")
-
-  def ringWorkAlarm():
-    logging.debug("SeringWorkAlarmtup")
-
-  def isTimeOver():
-    logging.debug("isTimeOver")
-
-  def startShortBreak():
-    logging.debug("startShortBreak")
-
-  def startLongBreak():
-    logging.debug("startLongBreak")
-
-  def ringBreakAlarm():
-    logging.debug("ringBreakAlarm")
+class Pomodoro:
   
-  def isTimeForLongBreak():
-    logging.debug("isTimeForLongBreak")
+  def __init__(self):
+    logging.debug("Construct pomodoro object")
+    self.setup()
+
+  def setup(self):
+    logging.debug("Setup")
+  
+  def checkState(self):
+    logging.debug("Check state")
+    # Create an object Timer which wait 1 second before calling the method checkState and keep track of it in the attribute _nextCheck
+    self._nextCheck = Timer(1, self.checkState)
+    # Start the timer
+    self._nextCheck.start()
+  
+  def start(self):
+    logging.debug("start Pomodoro")
+    self.checkState()
+
+  def startWork(self):
+    logging.debug("Start work")
+
+  def ringBreakAlarm(self):
+    logging.debug("Ring break alarm")
+
+  def startShortBreak(self):
+    logging.debug("Start short break")
+
+  def ringWorkAlarm(self):
+    logging.debug("Ring work alarm")
+
+  def startLongBreak(self):
+    logging.debug("Start long break")
+
+  def pause(self):
+    logging.debug("Pause")
+
+  def isPressed(self):
+    logging.debug("Is Pressed?")
+
+  def isTimeOver(self):
+    logging.debug("Is time over?")
+
+  def isTimeForLongBreak(self):
+    logging.debug("Is time for long break?")
+
+  def stop(self):
+    logging.debug("Stop Pomodoro")
+    # If a timer has been constructed and waiting for the next second
+    if self._nextCheck is not None:
+      # Cancel the timer
+      self._nextCheck.cancel()
+    
     
